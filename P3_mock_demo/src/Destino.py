c86aad9f9a2650fb954d1429c7d1334bf904e468
SergiCamos <sergi.camos@e-campus.uab.cat>
2020-05-17 09:41:11 +0200

    tarea realizada Sergi

---
+++ P3_mock_demo/src/Destino.py
@@ -0,0 +1,14 @@
+
+class Destino:
+    
+    def __init__(self, lugar, dias):
+        self.destinacion = lugar
+        self.dias = dias
+    
+    def informacion(self):
+        print (self.destinacion)
+        print (self.dias)
+        
+    
+        
+          
--- P3_mock_demo/src/Flights.py
+++ P3_mock_demo/src/Flights.py
@@ -1,4 +1,30 @@
+from vols import Vuelos
+
 class Flights:
 
-    def __init__(self):
-        pass
+    def __init__(self,Vuelo):
+        self.vuelos = Vuelo
+            
+    def informacion(self):
+        for elements in self.vuelos:
+            print(elements.Destinacion)
+
+    def añadirVuelo(self,codigo, destino, precio, pjs):
+        v = Vuelos(codigo, destino, precio, pjs)
+        self.vuelos.append(v)
+        for elements in self.vuelos:
+            print(elements.Codigo_vuelo)
+            
+    def eliminarVuelo(self,dest):
+        i = 0
+        for elements in self.vuelos:
+            if elements.Destinacion == dest:
+                borrar = i 
+            else:
+                i = i+1
+        
+        
+        self.vuelos.pop(borrar)        
+        for elements in self.vuelos:
+            print(elements.Destinacion)
+    
