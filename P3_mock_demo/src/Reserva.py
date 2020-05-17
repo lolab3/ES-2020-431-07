d8c7de6ce3592d638fddb22351b2ea4e7d20fb70
Avellaneda2000 <raul.avellaneda@e-campus.uab.cat>
2020-05-17 16:31:53 +0200

    "Confirmacion de pagos hecha"

---
+++ P3_mock_demo/src/Reserva.py
@@ -0,0 +1,6 @@
+class Reserva:
+    def __init__(self, id_reserva, preu):
+        self.id_reserva=id_reserva
+        self.preuReserva=preu
+
+    def confirmarReserva():
--- P3_mock_demo/src/Travel.py
+++ P3_mock_demo/src/Travel.py
@@ -4,17 +4,19 @@ Created on Fri May 15 23:40:39 2020
 
 @author: Usuario
 """
-
+from PaymentData import *
 from Flights import Flights
 from Destinos import Destinos
 
 class Travel:
     
     
-    def __init__(self,flight,destinos):
+    def __init__(self,flight,destinos, idViaje):
         self.f = flight
         self.d = destinos
-    
+        self.id=idViaje
+
+
     def informacion(self):
         for elements in self.f:
             print(elements.Codigo_vuelo)
@@ -31,4 +33,17 @@ class Travel:
     
     def añadirD(self,lugar, dias):
         self.d.añadirDestino(lugar, dias)
-        
+
+
+
+    def lista_viajeros(self, num, t: tv): #num de viajeros aparte del user >= 1
+        l = []
+        l.append(self)
+
+        if num >= 1:
+            self.set_NViajeros(num+1) #num de viajeros incluido el user
+            for v in range(self.NViajeros-1): #sin contar el user
+                l.append(t.Travellers('Viajero', v+1))#su DNI será un número
+
+        return l
+
